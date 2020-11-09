34e92acb617c91c4f088d91f902bea488e8dffc3
Souheil Chelfouh <trollfot@gmail.com>
2020-11-09 23:13:18 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import setup, find_packages
+
+
+version = "0.1"
+
+install_requires = [
+    'reg'
+]
+
+test_requires = [
+    'WebTest',
+    'pytest',
+]
+
+
+setup(
+    name='roughrider.workflow',
+    version=version,
+    author='Souheil CHELFOUH',
+    author_email='trollfot@gmail.com',
+    url='http://gitweb.dolmen-project.org',
+    download_url='http://pypi.python.org/pypi/roughrider.workflow',
+    description='Headless WSGI API',
+    long_description=(open("README.txt").read() + "\n" +
+                      open(os.path.join("docs", "HISTORY.txt")).read()),
+    license='ZPL',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python:: 3 :: Only',
+    ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['roughrider',],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    extras_require={
+        'test': test_requires,
+    },
+)
